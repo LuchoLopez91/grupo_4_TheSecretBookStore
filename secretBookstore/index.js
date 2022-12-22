b9e5fab01edfae9ee9944bb887f6d8913fc18b50
Fabián Ariel <fsosaolea@gmail.com>
2022-12-22 18:05:41 -0300

    estructura creada

---
+++ secretBookstore/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3030;
+
+app.use(express.static("./public"));
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+});
+
+app.get("/home", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+});
+
+app.get("/product", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/product.html"))
+});
+
+app.get("/signin", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/signin.html"))
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+});
+
+app.get("/cart", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/cart.html"))
+});
+
+
+app.listen(port, () => {
+    console.log(`The server is ON at the port http://localhost:${port}`)
+});
