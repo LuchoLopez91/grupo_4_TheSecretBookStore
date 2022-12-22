c0de557acc8fdee4683342a6c7f3d489d1d2a07e
Fabián Ariel <fsosaolea@gmail.com>
2022-12-22 18:38:31 -0300

    path arreglado

--- secretBookstore/index.js
+++ secretBookstore/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const port = 3030;
 
-app.use(express.static("./public"));
+app.use(express.static(path.join(__dirname,"./public")));
 
 
 app.get("/", (req, res) => {
