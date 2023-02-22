f15fbb030b6f30f794003770abb98946639201af
Fabián Ariel <fsosaolea@gmail.com>
2023-02-22 20:58:47 -0300

    modificaciones menores, correcciones

--- app/src/controllers/productsController.js
+++ app/src/controllers/productsController.js
@@ -110,7 +110,7 @@ module.exports = {
   booksByGenres: (req, res) => {
     let genre = req.params.category;
     let inventory = books.filter(book => book.genre == genre);
-    res.render('./products/booksByGenres', {
+    res.render('./products/books-by-genres', {
       inventory,
       genre,
       doctitle: genre,
