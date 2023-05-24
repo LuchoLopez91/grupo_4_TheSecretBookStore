f65f051cb2a57f0d2277e55de9ee98c9cf3c51cc
Fabián Ariel <fsosaolea@gmail.com>
2023-05-24 17:59:46 -0300

    visits count hotfix

--- app/src/middlewares/bookVisitsCount.js
+++ app/src/middlewares/bookVisitsCount.js
@@ -5,7 +5,7 @@ module.exports = async (req, res, next) => {
 
     try {
         let visits = await Book.findByPk(id, { attribute: ["visits_count"] });
-        let count = visits.visits_count++
+        let count = visits.visits_count + 1;
         let updatedVisits = await Book.update({ visits_count: count }, {where: {id}});
         next();
     } catch (error) {
